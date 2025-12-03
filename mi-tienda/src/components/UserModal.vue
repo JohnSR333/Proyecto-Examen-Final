8e33047a5440767ac1208d4740cedf9fcbf24770
John <jhonstivensantiago@gmail.com>
2025-12-02 21:48:06 -0500

    Se actualizo la carpeta completa de components

--- mi-tienda/src/components/UserModal.vue
+++ mi-tienda/src/components/UserModal.vue
@@ -1,22 +1,53 @@
 <template>
-  <div class="bg-dark text-white p-3" style="width: 220px; min-height: 100vh;">
-    <h4 class="mb-4">Mi Tienda</h4>
-    <ul class="nav flex-column">
-      <li class="nav-item mb-2">
-        <router-link to="/dashboard/productos" class="nav-link text-white">Productos</router-link>
-      </li>
-    </ul>
+  <div class="modal fade" tabindex="-1" ref="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+
+        <div class="modal-header">
+          <h5 class="modal-title">Editar Usuario</h5>
+          <button class="btn-close" @click="close"></button>
+        </div>
+
+        <div class="modal-body">
+          <input v-model="form.nombre" class="form-control mb-2" placeholder="Nombre">
+          <input v-model="form.email" class="form-control mb-2" placeholder="Email">
+          <input v-model="form.password" class="form-control mb-2" placeholder="Contraseña">
+        </div>
+
+        <div class="modal-footer">
+          <button class="btn btn-secondary" @click="close">Cancelar</button>
+          <button class="btn btn-primary" @click="save">Guardar</button>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
-</script>
+import { Modal } from "bootstrap";
 
-<style scoped>
-.nav-link:hover {
-  background-color: #ff6b6b;
-  border-radius: 5px;
-}
-</style>
+export default {
+  data() {
+    return {
+      form: {},
+      modalObj: null
+    };
+  },
+  methods: {
+    open(user) {
+      this.form = { ...user };
+      this.modalObj = new Modal(this.$refs.modal);
+      this.modalObj.show();
+    },
+    close() {
+      this.modalObj.hide();
+    },
+    save() {
+      this.$emit("save", this.form);
+      this.close();
+    }
+  }
+};
+</script>
 
